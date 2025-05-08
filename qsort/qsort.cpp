fe725646b49bd8d1ae142fa2a6893db0e4568b65
Yuxing Zhang <yzhang@cs.toronto.edu>
2025-05-08 16:22:53 -0400

    Added a static threading strategy.

--- qsort/qsort.cpp
+++ qsort/qsort.cpp
@@ -3,17 +3,23 @@
 #include <thread>
 #include <random>
 
-/* Threading stragegy:
- * Threads are dynamically created for the recursive calls. The template
- * argument `N` controls whether such cals will be executed in the current
- * thread or a new thread. If the length of the subsequence is greater than
- * `N`, a new thread will be created. Note that this`N` is different from
- * the template argument `N` in the `test` function. Their relationship is:
- * N_qs = L / (2 * N_test). Roughly speaking, N_test is the number of threads
- * to be created, on average.
+/*
+ * The quicksort is followed by two multithreaded version.
+ *
+ * Threading stragegies:
+ * (The word "array" used here refers to a general sequential data structure
+ * that supports random access iterators.)
+ * In qs_mt, the array is first divided into 'N' chunks. A thread is created
+ * for each chunk.
+ *
+ * In qs_dt, hreads are dynamically created for the recursive calls. The template
+ * argument `N` controls whether such calls will be executed in the current
+ * thread or a new thread. A static int 'n' is defined as the length of the
+ * array to be sorted divided by '2 * N'. If the length of the subsequence is
+ * greater than `n`, a new thread will be created. Roughly speaking, `N`is the
+ * number of threads to be created, on average.
  */
-
-template<int N, class Iter>
+template<class Iter>
 void qs(Iter begin, Iter end) {
     if (begin == end) return;
     Iter l{begin + 1}, r{end - 1};
@@ -25,38 +31,81 @@ void qs(Iter begin, Iter end) {
         t = *l, *l = *r, *r = t;
     }
     t = *begin, *begin = *r, *r = t;
+    qs(begin, r);
+    qs(l, end);
+}
+
+template<int N, class Iter>
+void qs_mt(Iter begin, Iter end) {
+    Iter l, r;
+    qs(begin, begin + N - 1);
+    begin += N - 1;
+    typename Iter::value_type t;
+    std::thread ths[N - 1];
+    for (int i = N - 1; i > 0; --i) {
+        --begin;
+        l = begin + 1, r = end - 1;
+        while (true) {
+            while (l != end && *l <= *begin) ++l;
+            while (r != begin && *r >= *begin) --r;
+            if (r < l) break;
+            t = *l, *l = *r, *r = t;
+        }
+        t = *begin, *begin = *r, *r = t;
+        ths[i - 1] = std::thread{qs<Iter>, l, end};
+        end = r;
+    }
+    qs(begin, end);
+    for (auto &t : ths) t.join();
+}
+
+// Dynamic threading
+template<int N, class Iter>
+void qs_dt(Iter begin, Iter end) {
+    if (begin == end) return;
+    Iter l{begin + 1}, r{end - 1};
+    typename Iter::value_type t;
+    static int n = (end - begin) / (2 * N);
+    while (true) {
+        while (l != end && *l <= *begin) l++;
+        while (r != begin && *r >= *begin) r--;
+        if (r < l) break;
+        t = *l, *l = *r, *r = t;
+    }
+    t = *begin, *begin = *r, *r = t;
     std::thread th;
-    if ((r - begin) > N) th = std::thread{qs<N, Iter>, begin, r};
-    else qs<N>(begin, r);
-    qs<N>(l, end);
+    if ((r - begin) > n) th = std::thread{qs_dt<N, Iter>, begin, r};
+    else qs(begin, r);
+    if ((end - l) > n) qs_dt<N>(l, end);
+    else qs(l, end);
     if (th.joinable()) th.join();
 }
 
-template<int N, int L, class Cont>
-void test(Cont &cont) {
+template<class Iter>
+void test(void (*qs)(Iter, Iter), Iter begin, Iter end) {
     std::chrono::time_point<std::chrono::system_clock> start;
     std::chrono::system_clock::rep dur = 0;
     std::uniform_int_distribution<> randint{0, 100};
     std::mt19937 rng;
     for (int n = 0; n < 5; ++n) {
-        for (int &i : cont) i = randint(rng);
+        for (Iter i = begin; i != end; ++i) *i = randint(rng);
         start = std::chrono::system_clock::now();
-        qs<L / (2 * N)>(cont.begin(), cont.end());
+        qs(begin, end);
         dur += (std::chrono::system_clock::now() - start).count();
     }
-    std::cout << N << ' ' << dur / 5. << std::endl;
+    std::cout << dur / 5. << std::endl;
 }
 
 int main() {
-    const int L = 10000;
-    std::vector<int> v(L);
-    test<16, L>(v);
+    std::vector<int> v(10000);
+    test(qs_mt<16, std::vector<int>::iterator>, v.begin(), v.end());
+    test(qs_dt<16, std::vector<int>::iterator>, v.begin(), v.end());
 /*
     std::vector<int> v;
     v.reserve(16);
     for (int i = 0; i++ < 16; v.push_back(rand() % 100));
     for (int i : v) std::cout << i << ' '; std::cout << '\n';
-    qs<2>(v.begin(), v.end());
+    qs_dt<4>(v.begin(), v.end());
     for (int i : v) std::cout << i << ' '; std::cout << '\n';
 */
 }
